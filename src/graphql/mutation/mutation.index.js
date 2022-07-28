46427fb5eff6b2a72b9af34b95363b84c1056356
Yeon Jeffrey <yeonjeseo@hotmail.com>
2022-07-28 23:13:47 +0900

    graphql - passport 연동 중

--- src/graphql/mutation/mutation.index.js
+++ src/graphql/mutation/mutation.index.js
@@ -1,11 +1,24 @@
-import { GraphQLObjectType } from 'graphql';
+import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
 import { SignupUserTypeConfig } from './users/users.mutation.types.js';
 
 const MutationType = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
     signupUser: SignupUserTypeConfig,
+    login: {
+      name: 'login',
+      description: 'login with graphql mutation',
+      type: GraphQLString,
+      args: {
+        account: { type: new GraphQLNonNull(GraphQLString) },
+        password: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve: async (root, args, context, info) => {
+        const { account, password } = args;
+        console.log(account, password);
+        return '32r892h3jr9qjwr9d';
+      },
+    },
   },
 });
-
 export default MutationType;
