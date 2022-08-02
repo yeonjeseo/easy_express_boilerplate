437f6e4a1ebe93198ac14b5e4fba015d4a6a41ce
Yeon Jeffrey <yeonjeseo@hotmail.com>
2022-08-02 22:29:44 +0900

    fix: mutation login config 분리

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,15 +1,7 @@
 import { GraphQLSchema } from 'graphql';
 import MutationType from './mutation/mutation.index.js';
 import QueryType from './query/query.index.js';
-// const queryType = new GraphQLObjectType({
-//   name: 'Query',
-//   fields: {
-//     hello: {
-//       type: GraphQLString,
-//       resolve: () => 'Hello world!',
-//     },
-//   },
-// });
+
 export const graphQLSchema = new GraphQLSchema({
   query: QueryType,
   mutation: MutationType,
