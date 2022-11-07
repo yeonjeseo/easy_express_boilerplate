5513a1b627c8bc70574cbe971e3dbaf0f6e526b4
Yeon Jeffrey <yeonjeseo@hotmail.com>
2022-11-07 16:40:35 +0900

    test: check if test thorws error

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,25 +1,27 @@
 class Calculator {
-	constructor() {
-		this.value = 0;
-	}
-	set(number) {
-	this.value = number
-	}
-	clear() {
-		this.value = 0;
-	}
-	add (number) {
-		this.value += number;
-	}
-	substract (number) {
-		this.value -= number;
-	}
-	divide(number) {
-		this.value /= number;
-	}
-	multiply(number) {
-		this.value *= number;
-	}
+  constructor() {
+    this.value = 0;
+  }
+  set(number) {
+    this.value = number;
+  }
+  clear() {
+    this.value = 0;
+  }
+  add(number) {
+    const sum = this.value + number;
+    if (sum > 100) throw new Error('Value can not be greater than 100');
+    this.value += number;
+  }
+  substract(number) {
+    this.value -= number;
+  }
+  divide(number) {
+    this.value /= number;
+  }
+  multiply(number) {
+    this.value *= number;
+  }
 }
 
-module.exports = Calculator
+module.exports = Calculator;
