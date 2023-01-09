f4a1a6e545936d9fb88c69a4c9f2c636f9a6d666
Yeon Jeffrey <yeonjeseo@hotmail.com>
2023-01-09 10:55:11 +0900

    fi

--- src/config/general.config.js
+++ src/config/general.config.js
@@ -30,7 +30,15 @@ const config = {
       logging: false,
       timezone: '+09:00',
     },
-    development: {},
+    development: {
+      username: envVars.PROD_DB_USERNAME,
+      password: envVars.PROD_DB_PASSWORD,
+      database: envVars.PROD_DB_DATABASE,
+      host: envVars.PROD_DB_HOST,
+      dialect: 'mysql',
+      logging: false,
+      timezone: '+09:00'
+    },
     production: {
       username: envVars.PROD_DB_USERNAME,
       password: envVars.PROD_DB_PASSWORD,
