723eeb185d354109c8a474a46d81b484bcf7a93d
Yeon Jeffrey <yeonjeseo@hotmail.com>
2023-03-30 00:02:57 +0900

    fix: removed unused routes

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -1,19 +1,22 @@
 import multer from 'multer';
 
-const noFileUpload = multer({
-  storage: multer.diskStorage({
-    destination: (req, file, callback) => callback(null, `${process.cwd()}/uploads`),
-    filename: (req, file, callback) => {
-      callback(null, file.originalname);
-    },
-  }),
-});
+// const noFileUpload = multer({
+//   storage: multer.diskStorage({
+//     destination: (req, file, callback) => callback(null, `${process.cwd()}/uploads`),
+//     filename: (req, file, callback) => {
+//       callback(null, file.originalname);
+//     },
+//   }),
+// });
 
-const noFileUploadSingle =  noFileUpload.none();
+const noFile = multer();
 
-const noFileUploadMiddleware = async (req, res, next) => noFileUploadSingle(req, res, error => {
+const noFileUpload =  noFile.none();
+
+const noFileUploadMiddleware = async (req, res, next) => noFileUpload(req, res, error => {
   try {
     if (error) throw error;
+    req.body = JSON.parse(JSON.stringify(req.body))
     return next();
   } catch (e) {
     next(e)
